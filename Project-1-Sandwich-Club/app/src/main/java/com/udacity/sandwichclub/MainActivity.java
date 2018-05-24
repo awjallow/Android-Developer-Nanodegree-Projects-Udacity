164dad958607ff06f1eaffcce31f4dd6acd72179
Amadou Jallow <wuri1@live.com>
2018-05-24 02:18:07 -0400

    added 'up' navigation, and customized style for listview items

--- Project-1-Sandwich-Club/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ Project-1-Sandwich-Club/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -1,12 +1,18 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +22,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, sandwiches);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, sandwiches){
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+                View view = super.getView(position, convertView, parent);
+                TextView tv = view.findViewById(android.R.id.text1);
+
+                // Set properties for each ListView item like size, textcolor, caps
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP,22);
+                tv.setAllCaps(true);
+                tv.setTextColor(Color.rgb(15, 6, 6));
+
+                // Return the view
+                return view;
+            }
+        };
 
         // Simplification: Using a ListView instead of a RecyclerView
         ListView listView = findViewById(R.id.sandwiches_listview);
